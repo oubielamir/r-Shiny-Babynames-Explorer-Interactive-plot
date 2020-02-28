b66ba334574413d325fc4d4ad0c60c2e3e59ce89
oubielamir <oubielamir@Oubis-MacBook-Pro.local>
2020-02-28 17:05:16 -0500

    R code with plotly and DT package

---
+++ babynames_plotly.R
@@ -0,0 +1,77 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(babynames)
+library(plotly)
+library(DT)
+
+
+
+ui <- fluidPage(
+  
+  titlePanel('Baby Names Explorer'),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      textInput('name', 'Enter Name')
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        
+        tabPanel(plotlyOutput('plot_names')),
+        
+        tabPanel(DTOutput('table_names'))
+        
+      )
+      
+      
+    )
+  )  
+  
+  
+)
+
+
+server <- function(input, output, session) {
+  
+  
+  
+  plot <- function() {
+    
+    babynames %>% filter(name == input$name) 
+  }
+  
+  output$plot_names <- renderPlotly({
+    
+    ggplot(plot(), aes(year, prop, color = sex)) +geom_col()
+  })
+  
+  output$table_names <- renderDT({
+    
+    plot()
+  })
+  
+  
+  
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
